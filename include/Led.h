ab42578e203a0b613921cd29b85cd37fae1433a4
G00Z_G00Z <edygomezgg4@gmail.com>
2022-09-24 13:52:43 -0500

    Added pwm

--- include/Led.h
+++ include/Led.h
@@ -1,18 +1,45 @@
 #pragma once
 
-/**
- * @brief Led class for handling on and off state
- *
- */
-class Led
+namespace Leds
 {
 
-private:
-    int pin;
+    /**
+     * @brief Led class for handling on and off state
+     *
+     */
+    class Led
+    {
 
-public:
-    Led();
-    Led(int pin);
-    void on();
-    void off();
+    protected:
+        int pin;
+
+    public:
+        Led();
+        Led(int pin);
+        virtual void on();
+        virtual void off();
+    };
+
+    /**
+     * @brief int that goes from 0 to 255
+     *
+     */
+    using dutyCycle_t = unsigned char;
+
+    /**
+     * @brief Class that handles Led that has PWM
+     *
+     */
+    class LedPWM : public Led
+    {
+
+    public:
+        LedPWM();
+        LedPWM(int pwmPin);
+        void on() override;
+        void off() override;
+
+        void setDutyCicle(dutyCycle_t duty);
+    };
+
 };
--- src/Led.cpp
+++ src/Led.cpp
@@ -1,9 +1,20 @@
 #include <Led.h>
 #include <Arduino.h>
 
+using namespace Leds;
+
 Led::Led() : pin(0) {}
 
 Led::Led(int pin) : pin(pin) { pinMode(pin, OUTPUT); }
 
 void Led::on() { digitalWrite(pin, HIGH); }
 void Led::off() { digitalWrite(pin, LOW); }
+
+LedPWM::LedPWM() : Led() {}
+
+LedPWM::LedPWM(int pwmPin) : Led(pwmPin) {}
+
+void LedPWM::on() { analogWrite(pin, 255); }
+void LedPWM::off() { analogWrite(pin, 0); }
+
+void LedPWM::setDutyCicle(Leds::dutyCycle_t duty) { analogWrite(pin, duty); }
