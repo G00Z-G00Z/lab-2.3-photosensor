26d69dd426f411801663c47064e650791994ef07
G00Z_G00Z <edygomezgg4@gmail.com>
2022-09-24 13:05:39 -0500

    Added teacher library to project

---
+++ lib/LightSensor/src/lightSensor.cpp
@@ -0,0 +1,18 @@
+/*This is where you write the code you want to run*/
+#include "Arduino.h"
+#include "ligthSensor.h"
+
+// Read transducer data
+int readLDRData(int analogPort)
+{
+  int sensorReading = analogRead(analogPort);
+  return sensorReading;
+}
+// Provide a brightness value from current LDR data and circuit's ADC range
+int setBrightness(int transducerValue, int minADC, int maxADC)
+{
+  // map current LDR data ADC range to the 255-0 scale Arduino can use to
+  // output analog volage
+  int brightness = map(transducerValue,minADC,maxADC, 255,0);
+  return brightness;
+}
