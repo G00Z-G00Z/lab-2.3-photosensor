2ec50089eb14992932ee8fe8120d57b8d63d7d20
G00Z_G00Z <edygomezgg4@gmail.com>
2022-09-24 14:25:08 -0500

    Made inheritance with display

--- include/Display.h
+++ include/Display.h
@@ -1,2 +1,15 @@
 #pragma once
 #include <LiquidCrystal.h>
+
+namespace Display
+{
+
+    class CustomDisplay : public LiquidCrystal
+    {
+
+    public:
+        CustomDisplay(uint8_t rs, uint8_t en, uint8_t d4, uint8_t d5, uint8_t d6, uint8_t d7)
+            : LiquidCrystal(rs, en, d4, d5, d6, d7) {}
+    };
+
+};
