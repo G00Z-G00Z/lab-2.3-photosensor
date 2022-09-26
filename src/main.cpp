ac13bce8587ef60cb130f71f0e70ab7db4dfd194
G00Z_G00Z <edygomezgg4@gmail.com>
2022-09-26 10:21:44 -0500

    Normal set up for serial data

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,16 @@
 #include <Arduino.h>
 
-void setup() {
+void setup()
+{
   // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.println("Hello world");
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+  static int i = 0;
+
+  Serial.println(String(i++) + ": Hola");
+  delay(1000);
 }
