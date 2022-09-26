e5a8afedb3722f36c9e7ed6017f926905acc9c52
G00Z_G00Z <edygomezgg4@gmail.com>
2022-09-26 12:00:28 -0500

    Read from serial

--- serial-reader/read-serial.py
+++ serial-reader/read-serial.py
@@ -1,12 +1,14 @@
 import sys
+import serial
 
 PORT_FILE_NAME = "arduino.port.txt"
+BAUD_RATE = 9600
 
 
 def exit_error():
     sys.exit(1)
 
-def get_port_name_from_text_file():
+def get_port_name_from_text_file() -> None:
     """
     Gets the port name from the file
     """
@@ -22,11 +24,27 @@ def get_port_name_from_text_file():
         print(f"Please make a file named {PORT_FILE_NAME} and put the port in which the arduino is connected")
         exit_error()
 
+def read_port(port_name : str) -> None: 
+
+    try:
+        with serial.Serial(port_name, BAUD_RATE, timeout=0) as ser:
+            for i in range(10):
+                byte_ = ser.readline()
+                string = byte_.decode()
+                stripped = string.strip()
+                print(byte_)
+                print(string)
+                print(stripped)
+    except serial.SerialException:
+        print("No se pudo leer el serial")
+        print("Quiza no esta conectado a la compu")
+        exit_error()
         
 
 def main():
     port_name = get_port_name_from_text_file()
     print(f"Your selected port was {port_name}")
+    read_port(port_name)
     pass
 
 if __name__ == '__main__':
