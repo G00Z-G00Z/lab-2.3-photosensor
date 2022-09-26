02bb528ecd3b7b4c8ffe658efb1b7aba3590e54f
G00Z_G00Z <edygomezgg4@gmail.com>
2022-09-26 11:08:00 -0500

    Added list serial ports

--- serial-reader/read-serial.py
+++ serial-reader/read-serial.py
@@ -1,7 +1,21 @@
+import serial
+import serial.tools.list_ports
+
+SERIAL_PORT = "COM4"
+
+def list_serial_ports():
+    print(list(serial.tools.list_ports.comports()))
+    
 
 
 def main():
-    print("Hello world")
+    
+    list_serial_ports()
+
+    # with serial.Serial(SERIAL_PORT, 9600, timeout=1) as ser:
+        # line = ser.readline()
+        # print(line)
+
 
 
 
@@ -10,3 +24,17 @@ if __name__ == '__main__':
     main()
 
 
+
+    
+
+
+
+
+
+
+
+
+
+
+
+    
