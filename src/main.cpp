3b57611cd4ab8b435821a72db34a88161659f6b4
G00Z_G00Z <edygomezgg4@gmail.com>
2022-09-26 15:12:42 -0500

    Put btn, led and lcd

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,24 @@
 #include <Arduino.h>
+#include <Display.h>
+#include <Button.h>
+#include <Led.h>
+
+#define PIN_LED_PWM 10
+#define BTN_PIN 10
+
+Buttons::Button btn(BTN_PIN);
+
+Leds::LedPWM led(PIN_LED_PWM);
+
+// Pins del lcd
+const int rs = 30,
+          en = 31,
+          d4 = 32,
+          d5 = 33,
+          d6 = 34,
+          d7 = 35;
+
+Display::CustomDisplay lcd(rs, en, d4, d5, d6, d7);
 
 void setup()
 {
@@ -9,8 +29,4 @@ void setup()
 
 void loop()
 {
-  static int i = 0;
-
-  Serial.println(String(i++) + ": Hola");
-  delay(1000);
 }
