fbad789c5ae38f4a02daec4a8dce3f9e906c7f0f
G00Z_G00Z <edygomezgg4@gmail.com>
2022-09-26 21:09:27 -0500

    Made figure show

--- serial-reader/linear-regression.py
+++ serial-reader/linear-regression.py
@@ -1,4 +1,6 @@
 from utils import file_exists, exit_error
+import pandas as pd
+import matplotlib.pyplot as plt
 
 
 SENSOR_DATA_FILENAME = "sensor_data.csv"
@@ -7,6 +9,19 @@ def main():
     if (not file_exists(SENSOR_DATA_FILENAME)):
        print(f"{SENSOR_DATA_FILENAME} doesnt exit")
        exit_error()
+       
+    sensor_table = pd.read_csv(SENSOR_DATA_FILENAME)
+    
+    plt.figure(1)
+    sensor_table.plot(x="distance", y="voltage")
+    plt.savefig("figura.png")
+
+    
+    
+    
+
+
+
         
 
 if __name__ == '__main__':
