131359e90fc84e30a9a5050a65fcae62209d73c1
G00Z_G00Z <edygomezgg4@gmail.com>
2022-09-26 19:39:01 -0500

    Corrected bug for datacollecto

--- src/DataCollector.cpp
+++ src/DataCollector.cpp
@@ -5,12 +5,12 @@ static const float reference_voltage = 5.0f;
 
 using namespace DataCollector;
 
-void print_labels()
+void DataCollector::print_labels()
 {
     Serial.println("distance, voltage");
 }
 
-void print_serial_data(int distance_cm, RBD::LightSensor &sensor)
+void DataCollector::print_serial_data(int distance_cm, RBD::LightSensor &sensor)
 {
     float voltage = ((float)sensor.getPercentValue() / 100.0f) * reference_voltage;
     Serial.println(String(distance_cm) + "," + String(voltage));
