ff77c902fe5798fc3779925091056094f525bfc5
G00Z_G00Z <edygomezgg4@gmail.com>
2022-09-26 21:43:19 -0500

    Made linear regression

--- serial-reader/linear-regression.py
+++ serial-reader/linear-regression.py
@@ -1,6 +1,7 @@
 from utils import file_exists, exit_error
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
 
 
 SENSOR_DATA_FILENAME = "sensor_data.csv"
@@ -13,8 +14,24 @@ def main():
     sensor_table = pd.read_csv(SENSOR_DATA_FILENAME)
     
     plt.figure(1)
-    sensor_table.plot(x="distance", y="voltage")
+    sensor_table.plot( x="voltage", y="distance",grid=True, title="distance vs voltage")
+    
+    distance = sensor_table.loc[:, "distance"] .to_numpy().reshape(-1,1)
+    voltage = sensor_table.loc[:, "voltage"] .to_numpy().reshape(-1,1)
+
+    linear_model = LinearRegression()
+    linear_model.fit(voltage, distance)
+
+    distance_predicts = linear_model.predict(voltage)  
+    R2 = linear_model.score(distance, distance_predicts)
+    print(f"R2 = {R2}")
+    coeffs = linear_model.coef_
+    print(f"Coeefs = {coeffs}")
+    print(f"inter = {linear_model.intercept_}")
+    plt.plot(voltage, distance_predicts)
     plt.savefig("figura.png")
+     
+
 
     
     
